a8eac95647c2e5263d22e7bbea50237c8afc9817
Rifa Tasfiya <93447807+Rifa-Tasfiya@users.noreply.github.com>
2023-01-11 22:18:13 +0600

    Add files via upload

---
+++ Swap Two Numbers Using Two Reg.asm
@@ -0,0 +1,56 @@
+; Swap two numbers, using a maximum of 3 registers.
+
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA
+a db 'Enter the fisrt number:$ '
+b db 0dh, 0ah, 'Enter the second number:$ '
+.CODE
+MAIN PROC
+
+;iniitialize DS
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+; enter your code here
+
+LEA DX, a
+MOV AH, 09
+INT 21h
+
+MOV AH, 01
+INT 21h
+
+MOV BX, AX   
+
+LEA DX, b
+MOV AH, 09
+INT 21h
+
+MOV AH, 01
+INT 21h
+
+MOV DX, AX
+MOV AX, BX
+MOV BX, DX 
+
+
+
+
+
+
+
+
+
+ 
+
+;exit to DOS
+               
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+    END MAIN
